67e441c94bc3e9b6e893241a3dcd6201e357f1bf
Stolen000 <sadzinh@gmail.com>
2025-04-24 01:03:12 +0100

    CSS and restructure do prices

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -3,13 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { TransportPricesComponent } from './transport-prices/transport-prices.component';
 import { TaxisComponent } from './taxis/taxis.component';
 import { MotoristaComponent } from './motorista/motorista.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 export const routes: Routes = [
     
     { path: "taxis" , component: TaxisComponent },
     { path: "prices" , component: TransportPricesComponent },
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
     { path: "motorista", component: MotoristaComponent},
+    { path: "dashboard", component: DashboardComponent},
 ]; 
 
 @NgModule({
