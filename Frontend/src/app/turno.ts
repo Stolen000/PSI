058b9717da947c64133334b1d8889596e79eb022
Stolen000 <sadzinh@gmail.com>
2025-05-09 16:16:46 +0100

    Turno, ID no frontend + viagens_realizadas em ambos

--- Frontend/src/app/turno.ts
+++ Frontend/src/app/turno.ts
@@ -1,9 +1,11 @@
 
 export interface Turno {    
+    _id: string;
     motorista_id: string;
     taxi_id: string;
     inicio_turno: Date;
     fim_turno: Date;
+    viagens_realizadas: number;
   }
   
   
