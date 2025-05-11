a0415d25e0952818562984168fa0cad405abeec4
GuilhermeMD10 <guilherme.md.off@gmail.com>
2025-05-12 00:27:36 +0100

    tudo embora

--- Frontend/src/app/price.ts
+++
@@ -1,5 +0,0 @@
-export interface Price {
-    basic_price: string;
-    luxurious_price: string;
-    nocturne_tax: string;  
-  }
--- frontend/src/app/morada.ts
+++
@@ -1,7 +0,0 @@
-export interface Morada {
-    numero_porta: number;
-    rua: string;
-    codigo_postal: string;
-    localidade: string;
-  }
-  
